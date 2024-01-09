16ba52cb29c69a65544e95150970a0f5ee93adf1
David-Najda <david.j.najda@gmail.com>
2024-01-09 19:29:02 +0100

    Moved functions to separate .py file
    Moved the train_epoch and evaluation functions to the 'CNN_functions.py' file found within the CNN_model folder.

---
+++ models/CNN_model/CNN_functions.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+def train_epoch(model, loader, optimizer, device='cpu'):
+    model.to(device)
+    model.train() # specifies that the model is in training mode
+
+    losses = []
+
+    for batch in loader:
+        x = batch[0]
+        y = batch[1]
+        x, y = x.float().to(device), y.float().to(device)
+
+        # Model prediction
+        preds = model(x)
+
+        # MSE loss function
+        loss = nn.MSELoss()(preds, y)
+
+        losses.append(loss.cpu().detach())
+
+        # Backpropagate and update weights
+        loss.backward()   # compute the gradients using backpropagation
+        optimizer.step()  # update the weights with the optimizer
+        optimizer.zero_grad(set_to_none=True)   # reset the computed gradients
+
+    losses = np.array(losses).mean()
+
+    return losses
+
+
+def evaluation(model, loader, device='cpu'):
+    model.to(device)
+    model.eval() # specifies that the model is in evaluation mode
+
+    losses = []
+
+    with torch.no_grad():
+        for batch in loader:
+            x = batch[0]
+            y = batch[1]
+            x, y = x.float().to(device), y.float().to(device)
+
+            # Model prediction
+            preds = model(x)
+
+            # MSE loss function
+            loss = nn.MSELoss()(preds, y)
+            losses.append(loss.cpu().detach())
+
+    losses = np.array(losses).mean()
+
+    return losses
