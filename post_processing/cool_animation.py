2726fc5b924ab8b7b6b50ef2e7596a8ea8634e0c
Lucas <L.J.R.Terlinden-Ruhl@student.tudelft.nl>
2024-01-11 14:42:32 +0000

    Trained and saved conv lstm model, updated cool_animation to also plot the predictions, but not yet denormalized

--- post_processing/cool_animation.py
+++ post_processing/cool_animation.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib import animation
+import torch
 
-def plot_animation(sample, dataset, preds, title_anim, save = False):
+def plot_animation(sample, dataset, model, title_anim, model_who, scaler_x, scaler_y, device = 'cuda', save = False):
     '''
 
     Parameters
@@ -11,10 +12,13 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
         Choose a sample to animate.
     dataset : torch.utils.data.Dataset
         dataset of train_val or test, in future, two datasets should be parameters.
-    prediction : torch.tensor
-        prediction of model on sample
+    model : class of model
+        model to predict on the sample
     title_anim : str
         title of the animation.
+    model_who : str
+        identifies which model is used to get a prediction
+        currently only contains "conv_lstm" a
     save : bool
         The default is False, if true will save the animation in the
         post_processing folder with the title in the format 'title_anim + sample'
@@ -24,8 +28,14 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
     None.
 
     '''
+    # bottom right graph could be the losses at each hour for both outputs
+    
     inputs = dataset[sample][0][0]
     targets = dataset[sample][1]
+    
+    if model_who == 'conv_lstm':
+        sample_list, _ = model(dataset[sample][0].unsqueeze(0).to(device)) # create a batch of 1?
+        preds = torch.cat(sample_list, dim=1).detach().cpu()[0] # remove batch
     
     feature1 = 0
     feature2 = 1
@@ -47,19 +57,15 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
     div1 = make_axes_locatable(ax1)
     cax1 = div1.append_axes('right', '5%', '5%')
 
-    static_tensor = inputs[0]  # index 0 refers to elevation
-    im1 = ax1.imshow(static_tensor, origin='lower')
+    static_tensor = scaler_x.inverse_transform(inputs.reshape(4, -1).T.cpu()[:, 0].reshape(64, 64)  # index 0 refers to elevation
+    im1 = ax1.imshow(static_tensor, cmap = 'terrain', origin='lower')
     cb1 = fig.colorbar(im1, cax=cax1)
     cb1.set_label(r'$m$')
     ax1.set_title('Elevation')
 
     # Subplot 4
-    div4 = make_axes_locatable(ax4)
-    cax4 = div4.append_axes('right', '5%', '5%')
-
     static_tensor2 = inputs[3]  # index 3 refers to boundary
-    im4 = ax4.imshow(static_tensor2, origin='lower')
-    cb4 = fig.colorbar(im4, cax=cax4)
+    ax4.imshow(static_tensor2, cmap = 'binary', origin='lower')
     ax4.set_title('Breach Location')
 
     # Subplot 2
@@ -67,7 +73,7 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
     cax2 = div2.append_axes('right', '5%', '5%')
 
     animated_tensor1 = targets[:, feature1]
-    im2 = ax2.imshow(animated_tensor1[0], origin='lower')
+    im2 = ax2.imshow(animated_tensor1[0], cmap = 'Blues', origin='lower')
     cb2 = fig.colorbar(im2, cax=cax2)
     cb2.set_label(f'{feature_dic_units[feature1]}')
     tx2 = ax2.set_title(f'Hour 0: {feature_dic[feature1]}')
@@ -77,7 +83,7 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
     cax3 = div3.append_axes('right', '5%', '5%')
 
     animated_tensor2 = targets[:, feature2]
-    im3 = ax3.imshow(animated_tensor2[0], origin='lower')
+    im3 = ax3.imshow(animated_tensor2[0], cmap = 'Greens', origin='lower')
     cb3 = fig.colorbar(im3, cax=cax3)
     cb3.set_label(f'{feature_dic_units[feature2]}')
     tx3 = ax3.set_title(f'Hour 0: {feature_dic[feature2]}')
@@ -86,8 +92,8 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
     div5 = make_axes_locatable(ax5)
     cax5 = div5.append_axes('right', '5%', '5%')
 
-    animated_tensor3 = preds[0, :, feature1]
-    im5 = ax5.imshow(animated_tensor3[0], origin='lower')
+    animated_tensor3 = preds[:, feature1]
+    im5 = ax5.imshow(animated_tensor3[0], cmap = 'Blues', origin='lower')
     cb5 = fig.colorbar(im5, cax=cax5)
     cb5.set_label(f'{feature_dic_units[feature1]}')
     tx5 = ax5.set_title(f'Prediction: {feature_dic[feature1]}')
@@ -96,8 +102,8 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
     div6 = make_axes_locatable(ax6)
     cax6 = div6.append_axes('right', '5%', '5%')
 
-    animated_tensor4 = preds[0, :, feature2]
-    im6 = ax6.imshow(animated_tensor4[0], origin='lower')
+    animated_tensor4 = preds[:, feature2]
+    im6 = ax6.imshow(animated_tensor4[0], cmap = 'Greens', origin='lower')
     cb6 = fig.colorbar(im6, cax=cax6)
     cb6.set_label(f'{feature_dic_units[feature2]}')
     tx6 = ax6.set_title(f'Prediction: {feature_dic[feature2]}')
@@ -107,7 +113,7 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
     cax8 = div8.append_axes('right', '5%', '5%')
 
     animated_tensor5 = animated_tensor1 - animated_tensor3
-    im8 = ax8.imshow(animated_tensor5[0], origin='lower')
+    im8 = ax8.imshow(animated_tensor5[0], cmap = 'jet', origin='lower')
     cb8 = fig.colorbar(im8, cax=cax8)
     cb8.set_label(f'{feature_dic_units[feature1]}')
     tx8 = ax8.set_title(f'Diff: {feature_dic[feature1]}')
@@ -117,7 +123,7 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
     cax9 = div9.append_axes('right', '5%', '5%')
 
     animated_tensor6 = animated_tensor2 - animated_tensor4
-    im9 = ax9.imshow(animated_tensor6[0], origin='lower')
+    im9 = ax9.imshow(animated_tensor6[0], cmap = 'jet', origin='lower')
     cb9 = fig.colorbar(im9, cax=cax9)
     cb9.set_label(f'{feature_dic_units[feature2]}')
     tx9 = ax9.set_title(f'Diff: {feature_dic[feature2]}')
@@ -177,5 +183,5 @@ def plot_animation(sample, dataset, preds, title_anim, save = False):
 
     plt.show()
     if save == True:
-        ani.save('post_processing/'+ title_anim + str(sample) + '.gif', writer='Pillow', fps=20)
+        ani.save('post_processing/'+ title_anim + str(sample) + '.gif', writer='Pillow', fps=5)
     return None
