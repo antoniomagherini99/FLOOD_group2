6bf8b96db8f1947c51ec5e06ded48f668f45172c
lterlindenruhl <L.J.R.Terlinden-Ruhl@student.tudelft.nl>
2024-01-17 10:38:17 +0100

    Small update to plot.py for function plot_test_loss
    Added xticks and a grid to make it easier to see which sample has a certain test loss. Also added a docstring to the functions

--- post_processing/plots.py
+++ post_processing/plots.py
@@ -24,19 +24,44 @@ def plot_losses(train_losses, validation_losses, model):
     plt.yscale('log')
     plt.title(model_who + 'training and validation losses')
     plt.xlabel('Epochs [-]')
-    plt.ylabel('Loss [-]')
+    plt.ylabel('Nromalized Loss [-]')
     plt.gca().yaxis.set_label_coords(-0.01, 0.5)
     plt.legend()
     plt.show()
 
     return None
 
-def plot_test_loss(dataset, model, train_val, device):
+def plot_test_loss(dataset, model, train_val, device = 'cuda'):
+    '''
+    Plot test loss for each sample of the requested dataset and model
+
+    Parameters
+    ----------
+    dataset : torch.utils.data.Dataset
+        Normalized dataset of train_val or test (1, 2, or 3).
+    model : class of model
+        Model to predict on the sample.
+    train_val : str
+        key for specifying what we are using the model for
+            'train_val' = train and validate the model
+            'test1' = test the model with dataset 1
+            'test2' = test the model with dataset 2
+            'test3' = test the model with dataset 3
+        Used in the title of the animation.
+    device : str
+        Device on which to perform the computations; 'cuda' is the default.
+
+    Returns
+    -------
+    None.
+
+    '''
     model_who = str(model.__class__.__name__)
     
     num_samples = len(dataset)
     sample_array = np.arange(0, num_samples)
     loss_sample = np.zeros((num_samples))
+    
     for sample in range(num_samples):
         target = dataset[sample][1]
         
@@ -45,11 +70,16 @@ def plot_test_loss(dataset, model, train_val, device):
             preds = torch.cat(sample_list, dim=1).detach().cpu()[0]  # remove batch
         elif model_who == 'UNet':
             preds = model(dataset[sample][0]).to(device).detach().cpu()
+            
         loss_sample[sample] = nn.MSELoss()(preds, target)
     
     plt.figure()
     plt.scatter(sample_array, loss_sample)
+    plt.xticks(range(num_samples))
     plt.xlabel('Sample Number')
     plt.ylabel('Normalized Loss [-]')
     plt.title(model_who + ': ' + train_val + ' losses for each sample')
+    plt.grid()
     plt.show()
+    
+    return None
