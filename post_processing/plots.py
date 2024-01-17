f8342f6fb72fa3a87e8546284bed8ff134ecf4d5
lterlindenruhl <L.J.R.Terlinden-Ruhl@student.tudelft.nl>
2024-01-17 09:54:13 +0100

    Added a function that shows the test loss per sample for each dataset in plots.py

--- post_processing/plots.py
+++ post_processing/plots.py
@@ -1,6 +1,9 @@
 # .py file containing the functions used for plotting results from CNN/ConvLSTM models for DSAIE FLOOD project.
 
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
 
 def plot_losses(train_losses, validation_losses, model):
     '''
@@ -13,12 +16,13 @@ def plot_losses(train_losses, validation_losses, model):
 
     Outputs: None 
     '''
+    model_who = str(model.__class__.__name__)
 
     plt.figure() 
     plt.plot(train_losses, color='blue', ls='--', label='Training')
     plt.plot(validation_losses, color='red', ls='--', label='Validation')
     plt.yscale('log')
-    plt.title(f'{model} training and validation losses')
+    plt.title(model_who + 'training and validation losses')
     plt.xlabel('Epochs [-]')
     plt.ylabel('Loss [-]')
     plt.gca().yaxis.set_label_coords(-0.01, 0.5)
@@ -26,3 +30,26 @@ def plot_losses(train_losses, validation_losses, model):
     plt.show()
 
     return None
+
+def plot_test_loss(dataset, model, train_val, device):
+    model_who = str(model.__class__.__name__)
+    
+    num_samples = len(dataset)
+    sample_array = np.arange(0, num_samples)
+    loss_sample = np.zeros((num_samples))
+    for sample in range(num_samples):
+        target = dataset[sample][1]
+        
+        if model_who == 'ConvLSTM':
+            sample_list, _ = model(dataset[sample][0].unsqueeze(0).to(device))  # create a batch of 1?
+            preds = torch.cat(sample_list, dim=1).detach().cpu()[0]  # remove batch
+        elif model_who == 'UNet':
+            preds = model(dataset[sample][0]).to(device).detach().cpu()
+        loss_sample[sample] = nn.MSELoss()(preds, target)
+    
+    plt.figure()
+    plt.scatter(sample_array, loss_sample)
+    plt.xlabel('Sample Number')
+    plt.ylabel('Normalized Loss [-]')
+    plt.title(model_who + ': ' + train_val + ' losses for each sample')
+    plt.show()
